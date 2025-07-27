10f4852213617a8432c602cafa49c0aed4541aa5
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-27 20:20:40 +0600

    numbers

---
+++ number.js
@@ -0,0 +1,18 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice= parseInt("50");
+var bananaPrice = parseFloat("50.5");
+
+// console.log(applePrice);
+// console.log(orangePrice + applePrice);
+// console.log(orangePrice + bananaPrice);
+
+// console.log(typeof orangePrice);
+// console.log(typeof chocolate);
+// console.log(typeof applePrice);
+
+var first = 0.2;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+
