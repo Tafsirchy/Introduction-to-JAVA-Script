376db2db4ca5408a256f115efcb90ef44eae47f9
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-27 13:08:12 +0600

    JS Variable

---
+++ variable.js
@@ -0,0 +1,25 @@
+/**
+ * VARIABLES ==> vary + able
+ * 5 things to declare a variable
+ * var age = 23; keyword variable_name = value ;
+ */
+
+// Numeric 
+var age = 23;
+// string
+var name = "Tafsir Chy"
+// boolean
+var isMarried = false;
+var isStudent = true;
+
+
+console.log(age);
+console.log(name);
+console.log(isMarried);
+console.log(isStudent);
+
+// To see what type of variable it is
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isMarried);
+console.log(typeof isStudent);
