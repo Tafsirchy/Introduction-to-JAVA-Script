c0efcc1288018672b24f50b0ce04c66ee4bfffe3
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-27 13:34:07 +0600

    Rules for writing any Variable

---
+++ naming.js
@@ -0,0 +1,40 @@
+/**Rules of Writing a Variable
+ * 1. Variable names can contain letters, digits, underscores, and dollar signs
+ * 2. Variable names must begin with a letter, underscore, or dollar sign
+ * 3. Variable names are case-sensitive (y and Y are different variables)
+ * 4. Reserved words cannot be used as variable names(Keywords, functions, objects, etc)
+ * 5. Variable names should be meaningful
+ * 6. Variable names should be short and descriptive
+ * 7. Variable names should be consistent
+ * 8. Variable names should be in camelCase
+ * 9. Variable names should be in snake_case
+ * 10. Variable names should be in PascalCase
+ */
+
+// 1. No keyword in variable name
+// 2. No space in variable name
+// 3. No special character in variable name
+// 4. No number at the beginning of variable name
+// 5. No dash in variable name
+// 6. No underscore in variable name
+// 7. No capital letter in variable name
+// 8. No qoute in variable name
+
+// How to write a long variable name:
+
+var my_first_name = "John"; // snake_case
+var myFirstName = "John"; // camelCase
+var MyFirstName = "John"; // PascalCase
+
+/**
+ * JavaScript Keywords (reserved words):
+ * 
+ * Keywords are a set of reserved words that have special meaning in JavaScript.
+ * They cannot be used as variable names.
+ * Cannot be used as names of variables, functions, objects,labels etc.
+ * Exmaples:
+ * var false = "John";
+ * var for = 15;
+ * Var name = "John";
+ * VAR name = "John";
+ */
